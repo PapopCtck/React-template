aaace6f68276bbc2678d2f7c534048db7990f63f
PapopCTCK <pao@almond.group>
2021-05-15 00:33:12 +0700

    [Update] Rewrite input in style component, Add support for prefix and suffix

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,108 +1,169 @@
-import { ChangeEvent, ChangeEventHandler, ReactElement, ReactNode, useState } from 'react';
+import { ChangeEvent, ChangeEventHandler, FocusEventHandler, forwardRef, ReactElement, ReactNode, Ref, useState } from 'react';
 import styled from '@emotion/styled';
 
 import { colorMix } from '../../utils';
 
-export interface IInput {
-  onChange?: ChangeEventHandler,
-  value?: string | number | readonly string[],
-  type?: string,
+interface Block {
+  block?: boolean,
 }
 
-export interface IInputWithLength {
+export interface IInputWithLength extends Block {
   label?: string,
   className?: string,
-  block?: boolean, 
-  suffix?: ReactNode, 
-  maxLength?: number, 
-  errorMessage?: string, 
+  prefix?: ReactNode,
+  suffix?: ReactNode,
+  maxLength?: number,
+  errorMessage?: string,
   value?: string | readonly string[],
   customValidate?: boolean,
   customValidateMessage?: string,
   required?: boolean,
+  placeholder?: string,
+  onChange?: ChangeEventHandler,
+  onBlur?: FocusEventHandler,
 }
 
-export const Input = styled.input`
+interface IStyledInput extends Block {
+  pref?: boolean,
+  suff?: boolean,
+}
+
+export const Input = styled.input<IStyledInput>(props => `
   padding: 10px;
-  border: 1px solid ${props => props.theme.borderColorBase};
-  border-radius: ${props => props.theme.borderRadiusBase};
+  border: 1px solid ${props.theme.borderColorBase};
+  border-radius: ${props.theme.borderRadiusBase};
   outline: none;
   transition: all .3s;
-  :focus {
-    border-color: ${props => props.theme.primaryColor};
-    box-shadow: ${props => `${props.theme.boxShadowInput} ${colorMix(props.theme.primaryColor,2)}`};
+  font-family: ${props.theme.fontFamily};
+  &:focus {
+    border-color: ${props.theme.primaryColor};
+    box-shadow: ${props.theme.boxShadowInput} ${colorMix(props.theme.primaryColor, 2)};
   }
-  &.block {
-      display: block;
-      margin: 10px 0;
-      width: 100%;
-      box-sizing: border-box;
-    }
-    & + .error-message {
-      max-height: 0;
+`,
+props => props.block && ({
+  display: 'block',
+  margin: '10px 0',
+  width: '100%',
+  boxSizing: 'border-box',
+}),
+props => props.pref && ({
+  paddingLeft: '40px', //prefix maxwidth + padding(left)
+}),
+props => props.suff && ({
+  paddingRight: '40px', //suffixfix maxwidth + padding(right)
+}),
+);
+
+
+const InputContainer = styled.div`
+  margin: 10px 0;
+  position: relative;
+  width: 100%;
+
+`;
+
+const ErrorMessage = styled.div`
+    max-height: 0;
       opacity: 0;
       overflow: hidden;
       transition: max-height 0.3s ease-in;
       transition: opacity 0.3s ease-in;
-  }
 `;
 
-const InputContainer = styled.div`
-  margin: 10px 0;
-  .label{
-    margin-bottom: 5px;
-  }
- .suffix-icon {
-    position: relative;
-    float: right;
-    top: -45px;
-    right: 10px;
+const Label = styled.div`
+ margin-bottom: 5px;
+`;
+
+const PrefixIcon = styled.span`
+    position: absolute;
+    top: 10px; //padding
+    bottom: 10px; //padding
+    height: fit-content;
+    left: 10px;
+    max-width: 25px;
+    overflow: hidden;
     &.disabled {
       color: ${props => props.theme.disabledColor};
     }
-  }
-  .text-count {
-    font-size: 12px;
-    margin-right: -5px;
-    float: right;
-    color: ${props => props.theme.textColorSecondary};
-  }
 `;
 
-export const InputWithLength = ({ 
+const SuffixIcon = styled.span`
+   position: absolute;
+    top: 10px; //padding
+    bottom: 10px; //padding
+    height: fit-content;
+    right: 10px;
+    max-width: 25px;
+    overflow: hidden;
+    &.disabled {
+      color: ${props => props.theme.disabledColor};
+    }
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
+const CountWrapper = styled.div<Block>`
+width: ${props => props.block ? '100%' : 'fit-content'};
+  position: relative;
+`;
+
+const TextCount = styled.div`
+text-align: right;
+    color: ${props => props.theme.textColorSecondary};
+
+`;
+
+export const InputWithLength = forwardRef(({
   label,
   className,
-  block, suffix, 
+  block, suffix, prefix,
   maxLength, errorMessage, value,
-  customValidate,customValidateMessage, ...rest }: IInputWithLength): ReactElement => {
-  const [length,setLength] = useState(0);
+  customValidate, customValidateMessage,required, ...rest }: IInputWithLength, ref: Ref<HTMLInputElement>): ReactElement => {
+  const [length, setLength] = useState(0);
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     const { length } = e.target.value;
     setLength(length);
   };
+
   return (
     <InputContainer>
       {
-        label && <div className="label">{label}</div>
+        label && <Label>{label}</Label>
       }
-      <Input 
-        className={`${className} ${block ? 'block' : ''} ${customValidate ? 'showcustom' : ''}`}
-        onInput={handleInput}
-        value={value}
-        maxLength={maxLength}
-        {...rest}
-      />
-      {
-        errorMessage && <div className="error-message">{customValidate ? customValidateMessage : errorMessage}</div>
-      }
-      {
-        suffix && <span className="suffix-icon">
-          {suffix}
-        </span>
-      }
-      { maxLength && <span className="text-count">
-        {`${value?.length ? value.length : length}/${maxLength}`}
-      </span>}
-    
+      <CountWrapper>
+        <InputWrapper>
+          {
+            prefix && <PrefixIcon>
+              {prefix}
+            </PrefixIcon>
+          }
+          <Input
+            className={`${className}${customValidate && 'showcustom'}`}
+            block={block}
+            pref={!!prefix}
+            suff={!!suffix}
+            onInput={handleInput}
+            value={value}
+            maxLength={maxLength}
+            ref={ref}
+            required={required}
+            {...rest}
+          />
+          {
+            errorMessage && <ErrorMessage className="error-message">{customValidate ? customValidateMessage : errorMessage}</ErrorMessage>
+          }
+          {
+            suffix && <SuffixIcon>
+              {suffix}
+            </SuffixIcon>
+          }
+        </InputWrapper>
+        {maxLength && <TextCount>
+          {`${value?.length ? value.length : length}/${maxLength}`}
+        </TextCount>}
+      </CountWrapper>
     </InputContainer>
-  );};
+  );
+});
