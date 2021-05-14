b01d51e704ad5581ae359a0d3ce57f6c9fe9944d
PapopCTCK <pao@almond.group>
2021-05-15 00:34:15 +0700

    [Update] Rewrite Select in styled

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,16 @@
 import React, { createRef, CSSProperties, ReactElement, ReactNode } from 'react';
-import styled from '@emotion/styled';
+
 import { withTheme, Theme } from '@emotion/react';
 import { ChevronDown } from 'react-feather';
 
-import { colorMix } from '../../utils';
 import { Checkbox } from '../Checkbox/Checkbox';
+import * as styled from './StyledSelect';
+
+const { SelectCarret, SelectContainer, 
+  SelectedText, SelectOption, 
+  SelectOptionContainer, SelectOptions, 
+  StyledCustomSelectContainer, StyledSelectContainer, 
+} = styled;
 
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
@@ -45,107 +51,6 @@ export interface ICustomSelectState {
   showOptionList: boolean,
   defaultSelectText: ReactNode,
 }
-
-export const StyledSelectContainer = styled.div`
-  max-width: 400px;
-  position: relative;
-  &.block {
-    max-width: 100%;
-    display: block;
-    box-sizing: border-box;
-  }
-  .select-carret {
-    position: absolute;
-    right: 0.8rem;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-`;
-
-export const StyledCustomSelectContainer = styled.div`
-.custom-select-container {
-  text-align: center;
-  position: relative;
-  padding: 10px;
-  border: 1px solid ${props => props.theme.borderColorBase};
-  border-radius: ${props => props.theme.borderRadiusBase};
-  outline: none;
-  background: ${props => props.theme.componentBackgroundColor};
-  transition: .3s all;
-  &:focus {
-    box-shadow: ${props => `${props.theme.boxShadowInput} ${colorMix(props.theme.primaryColor, 2)}`};
-    border: 1px solid ${props => props.theme.primaryColor};
-  }
-  &.block {
-      display: block;
-      margin: 10px 0;
-      width: 100%;
-      box-sizing: border-box;
-    }
-    & + .error-message {
-      max-height: 0;
-      opacity: 0;
-      overflow: hidden;
-      transition: max-height 0.3s ease-in;
-      transition: opacity 0.3s ease-in;
-  }
-  &.active {
-    box-shadow: ${props => `${props.theme.boxShadowInput} ${colorMix(props.theme.primaryColor, 2)}`};
-    border: 1px solid ${props => props.theme.primaryColor};
-  }
-
-  &.disabled {
-    color: ${props => props.theme.disabledColor};
-    & ~ .shellselect-carret {
-      color: ${props => props.theme.disabledColor};
-    }
-  }
-
-  .selected-text {
-    margin-right: calc(0.8rem + 15px);
-    height: calc(1em + 5px);
-    text-align: left;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    font-weight: normal;
-    &.placeholder {
-      color: #404040;
-      font-weight: normal;
-    }
-  }
-
-  .select-options-container {
-    position: absolute;
-    width: 100%;
-    z-index: 1000;
-    top: calc(1em + 28px);
-    left: 0;
-    padding-bottom: 20px;
-    ul.select-options {
-      margin: 0;
-      padding: 10px;
-      text-align: left;
-      width: 100%;
-      background-color: ${props => props.theme.componentBackgroundColor};
-      box-shadow:${props => props.theme.boxShadowBase};
-      overflow: hidden;
-      transform: translate3d(0, 0, 0);
-      box-sizing: border-box;
-      li.custom-select-option {
-        list-style-type: none;
-        padding: 6px 10px;
-        background: ${props => props.theme.componentBackgroundColor};
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid ${props => props.theme.borderColorBase};
-        color: ${props => props.theme.textColorSecondary};
-      }
-    }
-  }
-}
-`;
-
 class CustomSelect extends React.Component<ICustomSelectProps,ICustomSelectState> {
   constructor(props: ICustomSelectProps) {
     super(props);
@@ -218,16 +123,16 @@ class CustomSelect extends React.Component<ICustomSelectProps,ICustomSelectState
     const isAll = allKeyword && ((value === allKeyword) || (React.Children.count(children) === 2));
     return (
       <StyledCustomSelectContainer ref={this.container} onClick={disabled ? undefined : this.handleListDisplay}>
-        <div className={`custom-select-container  ${disabled ? 'disabled' : ''} ${showOptionList ? 'active' : ''}`}
+        <SelectContainer disabled={disabled} active={showOptionList} className={showOptionList ? 'active' : ''}
         >
-          <div
-            className={showOptionList ? `selected-text active ${value === '' && 'placeholder'}` : `selected-text ${value === '' && 'placeholder'}`}
+          <SelectedText
+            className={`${showOptionList && 'active'} ${value === '' && 'placeholder'}`}
           >
             {defaultSelectText ? defaultSelectText : placeholder}
-          </div>
-          <div className="select-options-container">
+          </SelectedText>
+          <SelectOptionContainer>
             {showOptionList && !disabled && (
-              <ul className={`select-options ${showOptionList ? 'active' : ''}`}>
+              <SelectOptions className={`${showOptionList ? 'active' : ''}`}>
                 {Array.isArray(children) ? children.map((option, idx) => {
                   if (!option) {
                     return null;
@@ -235,9 +140,9 @@ class CustomSelect extends React.Component<ICustomSelectProps,ICustomSelectState
                   if (Array.isArray(option)) {
                     return option.map((ele, index) => {
                       if (ele && React.isValidElement(ele)) {
-                        return (<li
+                        return (<SelectOption
                           key={`${idx}-${index}`}
-                          className={`custom-select-option ${ele.props.disabled && 'disabled'}`}
+                          className={`${ele.props.disabled && 'disabled'}`}
                           name={name}
                           id={id}
                           value={ele.props.value}
@@ -245,7 +150,7 @@ class CustomSelect extends React.Component<ICustomSelectProps,ICustomSelectState
                         >
                           {showSelect && <Checkbox id={`${id}-checkbox${idx}-${index}`} type="box" className="custom-select-option" checked={isAll || (value === ele.props.value)} readOnly />}
                           <span className="custom-select-option">{ele.props.children}</span>
-                        </li>
+                        </SelectOption>
                         );
                       }
                       return null;
@@ -265,8 +170,8 @@ class CustomSelect extends React.Component<ICustomSelectProps,ICustomSelectState
                       </li>
                     );
                   }
-                }) : React.isValidElement(children) && <li
-                  className={`custom-select-option ${children.props.disabled && 'disabled'}`}
+                }) : React.isValidElement(children) && <SelectOption
+                  className={`${children.props.disabled && 'disabled'}`}
                   name={name}
                   id={id}
                   value={children.props.value}
@@ -274,16 +179,16 @@ class CustomSelect extends React.Component<ICustomSelectProps,ICustomSelectState
                 >
                   {showSelect && <Checkbox id={`${id}-checkbox`} className="custom-select-option" type="box" checked={isAll || (value === children.props.value)} readOnly />}
                   <span className="custom-select-option">{children.props.children}</span>
-                </li>}
-              </ul>
+                </SelectOption>}
+              </SelectOptions>
             )}
-          </div>
-        </div>
-        <span className="select-carret">
+          </SelectOptionContainer>
+        </SelectContainer>
+        <SelectCarret>
           {
             customCarret ? customCarret : <ChevronDown color={disabled ? this.props.theme?.disabledColor : this.props.theme?.textColor} />
           }
-        </span>
+        </SelectCarret>
       </StyledCustomSelectContainer>
     );
   }
@@ -292,7 +197,7 @@ class CustomSelect extends React.Component<ICustomSelectProps,ICustomSelectState
 const CustomSelectWithTheme = withTheme(CustomSelect);
 
 export const Select = ({ children, block, containerClassname,containerStyle, ...rest }: ISelect): ReactElement => (
-  <StyledSelectContainer className={`${block ? 'block' : ''} ${containerClassname}`} style={containerStyle}>
+  <StyledSelectContainer className={`${containerClassname}`} block={block} style={containerStyle}>
     <CustomSelectWithTheme {...rest}>
       {children}
     </CustomSelectWithTheme>
---
+++ src/components/Select/StyledSelect.ts
@@ -0,0 +1,104 @@
+import styled from '@emotion/styled';
+
+import { colorMix } from '../../utils';
+
+export interface ISStyledSelectContainer {
+  block?: boolean,
+}
+
+export interface ISelectContainer {
+  active?: boolean,
+  disabled?: boolean,
+}
+
+
+export const StyledSelectContainer = styled.div<ISStyledSelectContainer>(`
+  max-width: 400px;
+  position: relative;
+`,
+props => props.block && ({
+  width: '100%',
+  display: 'block',
+  boxSizing: 'border-box',
+}),
+);
+
+export const StyledCustomSelectContainer = styled.div``;
+
+export const SelectCarret = styled.span`
+    position: absolute;
+    right: 0.8rem;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
+export const SelectContainer = styled.div<ISelectContainer>(props => `
+ text-align: center;
+  position: relative;
+  padding: 10px;
+  border: 1px solid ${props.theme.borderColorBase};
+  border-radius: ${props.theme.borderRadiusBase};
+  outline: none;
+  background: ${props.theme.componentBackgroundColor};
+  transition: .3s all;
+  &:focus {
+    box-shadow: ${props.theme.boxShadowInput} ${colorMix(props.theme.primaryColor, 2)};
+    border: 1px solid ${props.theme.primaryColor};
+  }
+`,
+props => props.active && ({
+  boxShadow: `${props.theme.boxShadowInput} ${colorMix(props.theme.primaryColor, 2)}`,
+  border: `1px solid ${props.theme.primaryColor}`,
+}),
+props => props.disabled && ({
+  color: `${props.theme.disabledColor}`,
+  '& ~ .shellselect-carret': {
+    color: `${props.theme.disabledColor}`,
+  },
+}),
+);
+
+export const SelectedText = styled.div`
+    margin-right: calc(0.8rem + 15px);
+    height: calc(1em + 5px);
+    text-align: left;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-weight: normal;
+    &.placeholder {
+      color: #404040;
+      font-weight: normal;
+    }
+`;
+
+export const SelectOptionContainer = styled.div`
+    position: absolute;
+    width: 100%;
+    z-index: 1000;
+    top: calc(1em + 28px);
+    left: 0;
+    padding-bottom: 20px;
+`;
+
+export const SelectOptions = styled.ul`
+      margin: 0;
+      padding: 10px;
+      text-align: left;
+      width: 100%;
+      background-color: ${props => props.theme.componentBackgroundColor};
+      box-shadow:${props => props.theme.boxShadowBase};
+      overflow: hidden;
+      transform: translate3d(0, 0, 0);
+      box-sizing: border-box;
+`;
+
+export const SelectOption = styled.li`
+        list-style-type: none;
+        padding: 6px 10px;
+        background: ${props => props.theme.componentBackgroundColor};
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid ${props => props.theme.borderColorBase};
+        color: ${props => props.theme.textColorSecondary};
+`;
