3f9f628185e0d22312e2280a8babc8f3857e8508
PapopCTCK <pao@almond.group>
2021-05-15 00:35:33 +0700

    [Update] Update Input stories to use InputHtmlAttributes interface

--- src/stories/components/Input.stories.tsx
+++ src/stories/components/Input.stories.tsx
@@ -1,13 +1,14 @@
 import { Story, Meta } from '@storybook/react';
+import { InputHTMLAttributes } from 'react';
 
-import { Input, IInput } from '../../components';
+import { Input } from '../../components';
 
 export default {
   title: 'Components/Input',
   component: Input,
 } as Meta;
 
-const Template: Story<IInput> = (args) => <Input {...args} />;
+const Template: Story<InputHTMLAttributes<HTMLInputElement>> = (args) => <Input {...args} />;
 
 export const TextInput = Template.bind({});
 TextInput.args = {
