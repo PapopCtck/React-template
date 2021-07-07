30eda66b6fa20a4c73d1944cc29372c64cd21324
PapopCTCK <pao@almond.group>
2021-07-07 17:04:45 +0700

    [Create] Create test-utils for providers in testing

---
+++ src/__test__/test-utils.tsx
@@ -0,0 +1,18 @@
+import React, { FC, ReactElement } from 'react';
+import { render, RenderOptions, RenderResult } from '@testing-library/react';
+import { ThemeProvider } from '@emotion/react';
+import defaultTheme from '../themes/default';
+
+const AllTheProviders: FC = ({ children }) => (
+  <ThemeProvider theme={defaultTheme}>
+    {children}
+  </ThemeProvider>
+);
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+): RenderResult => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
