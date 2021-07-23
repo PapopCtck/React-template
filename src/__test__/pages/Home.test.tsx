8a604d8435fd69c96cd955c42b30993465abd4b1
PapopCTCK <dpprachaya@gmail.com>
2021-07-23 14:21:15 +0700

    [Fix] Fix home.test still include mocki18n

--- src/__test__/pages/Home.test.tsx
+++ src/__test__/pages/Home.test.tsx
@@ -2,9 +2,6 @@ import { render } from 'test-utils';
 import { Suspense } from 'react';
 
 import { Home } from '@/pages';
-import { setupI18nMock } from '../__mocks__/MockReactI18n';
-
-setupI18nMock();
 
 test('render without crashing',async () => {
   const { findByText } = render(<Suspense fallback={<div />}> <Home /></Suspense>);
