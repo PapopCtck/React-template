49e0a38d65f9e2177aedb83dd509f094f9836bac
Papop <dpprachaya@gmail.com>
2021-07-23 17:29:32 +0700

    [Fix] Rollback to webpack builder for storybook (#14)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,30 @@
+const path = require("path")
+
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
-  stories: [
+  "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
     "@storybook/addon-jest"
   ],
-  core: {
-    builder: "storybook-builder-vite"
-  },
-  async viteFinal(config, { configType }) {
-    config.resolve.alias = {
-      "@": "src"
+  "webpackFinal": async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+          "@": toPath("/src")
+        },
+      },
     }
-    return config;
   },
-  typescript: {
-    check: false,
-  }
 }
